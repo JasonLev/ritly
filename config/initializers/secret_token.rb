5e6f3631bad37132a6780282478644ead36495bb
JasonLev <jasond.levy@gmail.com>
2013-10-11 10:11:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RitlyApp::Application.config.secret_token = '937fa0d1f1fc17918b24cf1b42d563b298738c02e352a0eb176378aed533ef5a45162dde724830452b638d80ce1628ecb59ac555420804e51250b5f0f7ef2c92'
